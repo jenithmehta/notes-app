e482d221749e1d71fa9bf1fc413a19bae29fb1b4
jen-dev-basesolve <jenith.mehta@basesolve.com>
2023-09-10 19:21:08 +0530

    Added pydantic model validation for signup form data

--- website/auth.py
+++ website/auth.py
@@ -1,24 +1,30 @@
 """Authentication file"""
-from flask import Blueprint, render_template
+import pydantic
+from flask import Blueprint, render_template, request, jsonify
+from website.validation_models import Signup_validation
 
 auth = Blueprint("auth",__name__)
 
 @auth.route("/login", methods=["GET"])
 def login():
     """ login """
-    return render_template("login.html")
+    return render_template("login.html",user={"is_authenticated":True})
 
 @auth.route("/logout", methods=["GET"])
 def logout():
     """ logout """
-    return render_template("logut.html")
+    return render_template("logut.html",user={"is_authenticated":True})
 
-@auth.route("/signup", methods=["GET"])
+@auth.route("/sign-up", methods=["GET","POST"])
 def signup():
     """ signup """
-    return render_template("signup.html")
+    if request.method == "POST":
+        form_data = request.form
+        signup_data = Signup_validation(**form_data)
+        return signup_data.model_dump()
+    return render_template("sign_up.html",user={"is_authenticated":True})
 
 @auth.route("/", methods=["GET"])
 def home():
     """ home """
-    return render_template("home.html")
+    return render_template("home.html",user={"is_authenticated":True})
